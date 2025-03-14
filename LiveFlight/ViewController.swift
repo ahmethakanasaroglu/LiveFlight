10fcbad196303be61f2f82c5d9570beddf4f8ad8
asarogluahmethakan <asarogluahmethakan@gmail.com>
2025-03-14 16:10:25 +0300

    Initial Commit

---
+++ LiveFlight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveFlight
+//
+//  Created by Ahmet Hakan Asaroğlu on 14.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LiveFlightTests/LiveFlightTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiveFlightTests.swift
+//  LiveFlightTests
+//
+//  Created by Ahmet Hakan Asaroğlu on 14.03.2025.
+//
+
+import Testing
+@testable import LiveFlight
+
+struct LiveFlightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
