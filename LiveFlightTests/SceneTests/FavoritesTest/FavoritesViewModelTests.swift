9234a6b16cd23c024b4ac8eb8f466ab5e35a98d5
asarogluahmethakan <asarogluahmethakan@gmail.com>
2025-03-24 13:53:10 +0300

    MockCoreDataManager dosyası silindi, içeriği FavoritesViewModelTests'e eklendi

--- LiveFlightTests/SceneTests/FavoritesTest/FavoritesViewModelTests.swift
+++ LiveFlightTests/SceneTests/FavoritesTest/FavoritesViewModelTests.swift
@@ -79,3 +79,31 @@ class FavoritesViewModelTests: XCTestCase {
         XCTAssertTrue(viewModel.isFlightInFavorites(flight))
     }
 }
+
+
+class MockCoreDataManager: CoreDataManager {
+    var flightsInFavorites: [State] = []
+    
+    override func isFlightInFavorites(_ flight: State) -> Bool {
+        return flightsInFavorites.contains { $0.icao24 == flight.icao24 }
+    }
+    
+    override func addFlightToFavorites(_ flight: State) {
+        flightsInFavorites.append(flight)
+    }
+    
+    override func removeFlightFromFavorites(_ flight: State) {
+        flightsInFavorites.removeAll { $0.icao24 == flight.icao24 }
+    }
+    
+    override func fetchFavoriteFlights() -> [FlightEntity] {
+        return flightsInFavorites.map { flight in
+            let entity = FlightEntity(context: persistentContainer.viewContext)
+            entity.icao24 = flight.icao24
+            entity.callSign = flight.callSign
+            entity.latitude = flight.latitude!
+            entity.longitude = flight.longitude!
+            return entity
+        }
+    }
+}
