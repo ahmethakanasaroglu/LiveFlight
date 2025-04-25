0adbfc790b49d335ff38590ac3a3605568985763
asarogluahmethakan <asarogluahmethakan@gmail.com>
2025-04-25 16:10:07 +0300

    FlightAnnotation yorum satırı yapıldı

--- LiveFlight/Extensions/FlightAnnotation.swift
+++ LiveFlight/Extensions/FlightAnnotation.swift
@@ -1,29 +1,29 @@
+////
+////  FlightAnnotation.swift
+////  LiveFlight
+////
+////  Created by Ahmet Hakan Asaroğlu on 14.03.2025.
+////
 //
-//  FlightAnnotation.swift
-//  LiveFlight
+//import MapKit
 //
-//  Created by Ahmet Hakan Asaroğlu on 14.03.2025.
+//class FlightAnnotation: NSObject, MKAnnotation {
+//    var coordinate: CLLocationCoordinate2D
+//    var title: String?
+//    var subtitle: String?
+//    var trueTrack: Float? // Uçuş yönü burada tanımlandı
+//    var icao24: String?
+//    var callSign: String?
+//    var originCountry: String?
 //
-
-import MapKit
-
-class FlightAnnotation: NSObject, MKAnnotation {
-    var coordinate: CLLocationCoordinate2D
-    var title: String?
-    var subtitle: String?
-    var trueTrack: Float? // Uçuş yönü burada tanımlandı
-    var icao24: String?
-    var callSign: String?
-    var originCountry: String?
-
-    init(flight: State) {
-        self.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(flight.latitude ?? 0.0),
-                                                 longitude: CLLocationDegrees(flight.longitude ?? 0.0))
-        self.title = flight.callSign ?? "Bilinmeyen Uçuş"
-        self.subtitle = flight.originCountry ?? "Bilinmeyen Ülke"
-        self.trueTrack = flight.trueTrack //  TrueTrack değerini atadık
-        self.icao24 = flight.icao24 ?? "UNKNOWN"
-        self.callSign = flight.callSign
-        self.originCountry = flight.originCountry
-    }
-}
+//    init(flight: State) {
+//        self.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(flight.latitude ?? 0.0),
+//                                                 longitude: CLLocationDegrees(flight.longitude ?? 0.0))
+//        self.title = flight.callSign ?? "Bilinmeyen Uçuş"
+//        self.subtitle = flight.originCountry ?? "Bilinmeyen Ülke"
+//        self.trueTrack = flight.trueTrack //  TrueTrack değerini atadık
+//        self.icao24 = flight.icao24 ?? "UNKNOWN"
+//        self.callSign = flight.callSign
+//        self.originCountry = flight.originCountry
+//    }
+//}
